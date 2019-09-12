48a49cbcbf64e6443d2a49c6fa47ef97da94fbf6
moyfdzz <moy-881@hotmail.com>
2019-09-12 16:32:08 -0500

    Added HW7

---
+++ Racket/HW07.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+;; Complete the following functions and submit your file to Canvas.
+
+;; ========= IMPORTANT ==========
+;; Change the name of the file so that it contains your student ID or name.
+;; Do not change the names of the functions. The system will specifically look for the names used in this file. Changing the name of a function will automatically make you lose the points for that function.
+;; Do not change the number of arguments in the functions. Changing the number of arguments of a function will automatically make you lose the points for that function.
+;; Make sure that nothing additional is printed as the result of running this file. Avoid printing names, student IDs, or any other messages. If you print anything else, the system will wrongly grade your solutions.
+;; If your file cannot be loaded by the Racket interpreted, your grade will be zero. Then, submit only code that works.
+
+(define (exist? x lst)
+  	(display "Not yet implemented.")
+)
+
+(define (index x lst)
+	(display "Not yet implemented.")
+)
+
+(define (allDifferent? lst)
+	(display "Not yet implemented.")
+)
+
+(define (set x)
+	(display "Not yet implemented.")
+)
+
+(define (union x y)
+	(display "Not yet implemented.")
+)
+
+(define (intersect x y)
+  	(display "Not yet implemented.")
+)
+
+(define (det3x3 m)
+	(display "Not yet implemented.") 
+)
