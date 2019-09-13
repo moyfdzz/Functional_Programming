2112a2506df6b2f5b23285291d003a4d49549b37
moyfdzz <moy-881@hotmail.com>
2019-09-13 13:07:11 -0500

    Started HW07

--- Racket/HW07.rkt
+++ Racket/HW07.rkt
@@ -9,14 +9,33 @@
 ;; Make sure that nothing additional is printed as the result of running this file. Avoid printing names, student IDs, or any other messages. If you print anything else, the system will wrongly grade your solutions.
 ;; If your file cannot be loaded by the Racket interpreted, your grade will be zero. Then, submit only code that works.
 
+;P1 Does the element exist?
 (define (exist? x lst)
-  	(display "Not yet implemented.")
+  (if (null? lst)
+		#f
+		(if (= (car lst) x)
+			#t
+			(exist? x (cdr lst))
+		)
+	)
 )
 
+;(exist? 6 '(1 2 3 4 5))
+
+;P2 Getting the index of an element in a list
 (define (index x lst)
-	(display "Not yet implemented.")
+	(if (null? lst)
+		-1
+		(if (= (car lst) x)
+			0
+			(+ 1 (index x (cdr lst)))
+		)
+	)
 )
 
+;(index 10 '(5 10 15 20 25))
+
+;P3 All different
 (define (allDifferent? lst)
 	(display "Not yet implemented.")
 )
