2d8823727219089b29109c9c63d3364cf28a9a4a
Moisés Fernández <moy-881@hotmail.com>
2019-10-10 17:39:03 -0500

    Started Midterm Exam 02

---
+++ Haskell/ME2.hs
@@ -0,0 +1,25 @@
+module ME2 where
+
+-- Midterm Exam 02
+
+-- Enigmas
+
+-- 1 - Analyze the code
+enigmaA :: [Int] -> [Int]
+enigmaA n = error "Not implemented yet."
+
+enigmaB :: [Double] -> Double -> [(Bool, Bool)]
+enigmaB n x = error "Not implemented yet."
+
+enigmaC :: [Int] -> Int -> Int
+enigmaC n x = error "Not implemented yet."
+
+-- Problemas
+
+-- 2 - Removing the last n elements in a list
+removeNElementsAux :: Int -> [t] -> [t]
+removeNElementsAux
+
+removeNElements :: Int -> [t] -> [t]
+removeNElements _ [] = []
+removeNElements n lst = 
