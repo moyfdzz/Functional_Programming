4b2a2980a90ea300a1ec37310a43d78d0bca7149
Moisés Fernández <moy-881@hotmail.com>
2019-11-21 15:00:46 -0600

    Deleted folder

--- Erlang/hw13.erl
+++ Erlang/hw13.erl
@@ -10,20 +10,30 @@
 % Make sure that nothing additional is printed as the result of running this file. Avoid printing names, student IDs, or any other messages. If you print anything else, the system will wrongly grade your solutions.
 % If your file cannot be loaded by the Erlang compiler, your grade will be zero. Then, submit only code that works.
 
+% Moisés Fernández Zárate A01197049
+
 % Lists to represent stacks
-push(_, _) -> io:format("Not yet implemented.~n").
+push(N, []) -> [N];
+push(N, S) -> [N] ++ S.
 
-pop(_) -> io:format("Not yet implemented.~n").
+pop([]) -> io:format("Error: The stack is empty. \n");
+pop([_ | TS]) -> TS.
 
-peek(_) -> io:format("Not yet implemented.~n").
+peek([]) -> io:format("Error: The stack is empty. \n");
+peek([N | _]) -> N.
 
 % Matrix addition
-matrixSum(_, _) -> io:format("Not yet implemented.~n").
+matrixSum(A, B) -> lists:zipwith(fun (X, Y) -> lists:zipwith(fun (Z, W) -> Z + W end, X, Y) end, A, B).
 
 % Get balance
 % [{charge, 1000}, {charge, 2000}, {payment, 500}, {payment, 3000}, {charge, 3500}]
-getBalance(_) -> io:format("Not yet implemented.~n").
+getBalance([]) -> 0;
+getBalance([{Option, Amount} | RestOperations]) -> if 
+        Option == charge -> 
+            -Amount + getBalance(RestOperations);
+        true -> getBalance(RestOperations) + Amount
+    end.
 
 % Search in binary trees
-%[8, [5, [2, [], []], [7, [], []]], [9, [], [15, [11, [], []]]]]			
-search(_, _) -> io:format("Not yet implemented.~n").
+%[8, [5, [2, [], []], [7, [], []]], [9, [], [15, [11, [], []]]]]
+search(N, X) -> lists:member(N, lists:flatten(X)).
